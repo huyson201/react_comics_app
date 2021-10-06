05d4865e6908681c597e8326790a58b2173503c0
baongoc156 <phbaongoc2001@gmail.com>
2021-10-07 04:06:35 +0700

    xong phan design front end cho danh sách truyen

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,70 @@
-import React, { Component } from 'react'
-
+import React, { Component } from "react";
+import "./home.css";
+import { BsStars } from "react-icons/bs";
 export default class Home extends Component {
-    render() {
-        return (
-            <div>
-                Home page
-                Danh sách truyện
+  render() {
+    return (
+      <>
+        <div className="list-title">
+          <BsStars />
+          Truyện mới cập nhật
+        </div>
+        <div className="list-comic">
+          <div className="list-comic-item">
+            <img className="item-img" src="https://wcomic.site/upload/poster/comicid-4386.jpg"></img>
+            <div className="item-row">
+              <div className="item-new-chapter">Chap 50</div>
+              <div className="item-rate">5.0</div>
             </div>
-        )
-    }
+            <div className="item-name">Minh nhật chi kiếp</div>
+          </div>
+
+          <div className="list-comic-item">
+            <img className="item-img" src="https://wcomic.site/upload/poster/comicid-4386.jpg"></img>
+            <div className="item-row">
+              <div className="item-new-chapter">Chap 50</div>
+              <div className="item-rate">5.0</div>
+            </div>
+            <div className="item-name">Minh nhật chi kiếp</div>
+          </div>
+
+          <div className="list-comic-item">
+            <img className="item-img" src="https://wcomic.site/upload/poster/comicid-4386.jpg"></img>
+            <div className="item-row">
+              <div className="item-new-chapter">Chap 50</div>
+              <div className="item-rate">5.0</div>
+            </div>
+            <div className="item-name">Minh nhật chi kiếp</div>
+          </div>
+
+          <div className="list-comic-item">
+            <img className="item-img" src="https://wcomic.site/upload/poster/comicid-4386.jpg"></img>
+            <div className="item-row">
+              <div className="item-new-chapter">Chap 50</div>
+              <div className="item-rate">5.0</div>
+            </div>
+            <div className="item-name">Minh nhật chi kiếp</div>
+          </div>
+
+          <div className="list-comic-item">
+            <img className="item-img" src="https://wcomic.site/upload/poster/comicid-4386.jpg"></img>
+            <div className="item-row">
+              <div className="item-new-chapter">Chap 50</div>
+              <div className="item-rate">5.0</div>
+            </div>
+            <div className="item-name">Minh nhật chi kiếp</div>
+          </div>
+
+          <div className="list-comic-item">
+            <img className="item-img" src="https://wcomic.site/upload/poster/comicid-4386.jpg"></img>
+            <div className="item-row">
+              <div className="item-new-chapter">Chap 50</div>
+              <div className="item-rate">5.0</div>
+            </div>
+            <div className="item-name">Minh nhật chi kiếp</div>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
